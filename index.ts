0b449e10a5cf6839cd8fe90ebf2783da22583b43
Egon92 <90727274+Egon92@users.noreply.github.com>
2022-04-05 18:12:56 +0200

    Added signup and login endpoints

--- index.ts
+++ index.ts
@@ -18,7 +18,7 @@ app.get("/users", async (req, res) => {
   res.send(users);
 });
 
-app.post("/users", async (req, res) => {
+app.post("/signup", async (req, res) => {
   const newUser = await prisma.user.create({
     data: {
       firstname: req.body.firstname,
@@ -31,7 +31,59 @@ app.post("/users", async (req, res) => {
   });
   res.send(newUser);
 });
-
+app.get("/topics", async (req, res) => {
+  const topics = await prisma.topic.findMany({
+    include: {
+      shows: true,
+      users: { include: { episodes: true } },
+    },
+  });
+  res.send(topics);
+});
+app.post("/topics", async (req, res) => {
+  const newTopic = await prisma.topic.create({
+    data: {
+      name: req.body.name,
+    },
+  });
+  res.send(newTopic);
+});
+app.delete("/topics", async (req, res) => {
+  const deletedTopic = await prisma.topic.delete({
+    where: { id: 5 },
+  });
+  res.send(deletedTopic);
+});
+app.post("/shows", async (req, res) => {
+  const newShow = await prisma.show.create({
+    data: {
+      topicId: req.body.topicId,
+      title: req.body.title,
+      mediaProvider: req.body.mediaProvider,
+    },
+  });
+  res.send(newShow);
+});
+app.get("/shows", async (req, res) => {
+  const shows = await prisma.show.findMany();
+  res.send(shows);
+});
+app.post("/login", async (req, res) => {
+  //Check if the username exists
+  const user = await prisma.user.findFirst({
+    where: { username: req.body.username },
+  });
+  if (user) {
+    //Check if the password matches the user's password
+    if (user.password === req.body.password) {
+      res.send(user);
+    } else {
+      res.status(400).send({ error: "password not correct!" });
+    }
+  } else {
+    res.status(400).send({ error: "user not found" });
+  }
+});
 app.listen(PORT, () => {
   console.log(`Server is listening at http://localhost:${PORT}`);
 });
